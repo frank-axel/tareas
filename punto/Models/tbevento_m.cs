f8ad72c32f7a11da5eb10d0baa387f4895841cbe
frank-axel <axel_zorro1990@hotmail.com>
2015-06-09 15:42:45 -0400

    de hoy

---
+++ punto/Models/tbevento_m.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace punto.Models
+{
+    public partial class tbevento
+    {
+        public void prueba() {
+            this.estado = 1*5;
+        }
+    }
+    
+}
